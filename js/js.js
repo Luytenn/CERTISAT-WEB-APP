835b6f84fe2905d7fa3a0d933b2da687d640a7cc
efrain <efrain.yauri@hotmail.com>
2021-05-24 11:40:25 -0500

    cambios

--- js/js.js
+++ js/js.js
@@ -8,15 +8,7 @@ function myFunction() {
 
    document.getElementById("cambiarImagen").src="imagenes/ELEMENTOS-48.png";
    document.getElementById("cambiarImagen0").src="imagenes/ELEMENTOS-40.png";
-  /*
-    y.style.contains="";
-    y.style.position = "absolute";
-    y.style.top = "0";
-    y.style.left = "0";
-    y.style.width = "100%";
-    y.style.height = "100vh";
-    y.style.backgroundImage = "linear-gradient(to right, rgba(255,0,0,0), rgb(49, 119, 199))";
-*/
+ 
 
   y.style.backgroundImage = "url('imagenes/ELEMENTOS-02.png') ";
   y.style.position = "absolute"
@@ -129,10 +121,22 @@ document.addEventListener('mouseup', function(e) {
  function myFunction4() {
   console.log("Funciona");
   var x = document.getElementById("miServicio4");
-  
-   document.getElementById("cambiarImagen").src="imagenes/ELEMENTOS-48.png";
+  var y = document.getElementById("layer");
 
- 
+  var z = document.getElementById("background-image");
+
+   document.getElementById("cambiarImagen").src="imagenes/ELEMENTOS-48.png";
+   document.getElementById("cambiarImagen2").src="imagenes/ELEMENTOS-40.png";
+
+   z.style.zIndex = "initial";
+   y.style.backgroundImage = "url('imagenes/ELEMENTOS-02.png') ";
+  y.style.position = "absolute"
+  y.style.top = "0";
+  y.style.left = "0";
+  y.style.width = "100%";
+  y.style.height = "100%";
+  y.style.zIndex = "999";
+
    x.style.display = "block";
   x.style.opacity = "1";
   x.style.left = "25vh";
@@ -145,12 +149,21 @@ document.addEventListener('mouseup', function(e) {
  document.addEventListener('mouseup', function(e) {
    var container = document.getElementById("miServicio4");
    if (!container.contains(e.target)) {
+      console.log("ingreso outclick ")
        container.style.display = 'none';
-       document.getElementById("cambiarImagen").src="imagenes/ELEMENTOS-41.png";
-       document.getElementById("cambiarImagen2").src="imagenes/ELEMENTOS-46.png";
+       document.getElementById("cambiarImagen").src="imagenes/ELEMENTOS-40.png";
+       document.getElementById("cambiarImagen2").src="imagenes/ELEMENTOS-49.png";
+       m = document.getElementById("layer");
+       m.style.backgroundImage = "none";
+       console.log(" out of range ");
+
+       var z = document.getElementById("background-image");
+       z.style.zIndex = "2";
    }
   });
 
+
+  
 
   function myFunction5() {
     console.log("Funciona");
